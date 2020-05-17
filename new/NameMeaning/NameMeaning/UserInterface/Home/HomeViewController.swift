ffb17ccfabb93ef3c4ddbf27a2756191d8e14912
Vladimir Gogunsky <gogunsky.vladimir@gmail.com>
2020-05-17 22:28:35 +0300

    Partially added home screen design

---
+++ new/NameMeaning/NameMeaning/UserInterface/Home/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  NameMeaning
+//
+//  Created by Vladimir Gogunsky on 5/17/20.
+//  Copyright © 2020 Volodymyr Hohunskyi. All rights reserved.
+//
+import UIKit
+
+
+final class HomeViewController: UIViewController {
+
+}
---
+++ new/NameMeaning/NameMeaning/UserInterface/List/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  NameMeaning
+//
+//  Created by Vladimir Gogunsky on 5/17/20.
+//  Copyright © 2020 Volodymyr Hohunskyi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class ListViewController: UIViewController {
+
+}
